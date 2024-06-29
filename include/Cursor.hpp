390bdcd14deb2546c2ba9e1fd91df8c082e16a91
Farhan Majid <farhanmajid23@gmail.com>
2024-06-29 16:25:25 -0500

    Basic Buffer done.

---
+++ include/Cursor.hpp
@@ -0,0 +1,12 @@
+#ifndef CURSOR
+#define CURSOR
+
+class Cursor{
+    private:
+        
+    public:
+
+};
+
+
+#endif 
--- src/Buffer.cpp
+++ src/Buffer.cpp
@@ -22,5 +22,28 @@ void Buffer::deletechar(size_t position){
 }
 
 void Buffer::deleterange(size_t position, size_t range){
-    
+    //The second condition makes sure that the size is sufficient and the first condition together with the second one makes sure the position is within range and is far enough from the end to be accurate.
+    if(position < text.size() && position + range <= text.size()){
+        text.erase(position, range);
+    }
+}
+
+void Buffer::replaceChar(size_t position, char c){
+    if(position < text.size()){
+        text[position] = c;
+    }
+}
+
+void Buffer::replaceString(size_t position, std::string str){
+    if(position < text.size() && position + str.size() <= text.size()){
+        text.replace(position, str.size(), str);
+    }
+}
+
+std::string Buffer::getText(){
+    return text;
+}
+
+void Buffer::clear(){
+    text.clear();
 }
